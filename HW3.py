1383a19cdfbee76d63409d3626bbd6a673e6e951
brian ma <brian41005@gmail.com>
2017-06-04 17:38:06 +0800

    final modify

--- HW3.py
+++ HW3.py
@@ -6,6 +6,11 @@ import time
 from operator import add
 from datetime import datetime
 from geopy.geocoders import Nominatim
+APP_NAME = "BDA-HW3 103590450"
+conf = pyspark.SparkConf().setAppName(APP_NAME)
+conf = conf.setMaster("local[*]")
+sc = pyspark.SparkContext()
+geolocator = Nominatim()
 
 
 def row_split(string):
@@ -29,13 +34,10 @@ def get_name(line):
     return ('%s, %s' % (latitude, longitude), 1)
 
 
-if __name__ == '__main__'"
-    APP_NAME = "BDA-HW3 103590450"
-    conf = pyspark.SparkConf().setAppName(APP_NAME)
-    conf = conf.setMaster("local[*]")
-    sc = pyspark.SparkContext()
-    geolocator = Nominatim()
-
+if __name__ == '__main__':
+    text_file = open("Output.txt", "w")
+    output = ''
+    line = '#' * 100 + '\n'
     checking_local_dedup = sc.textFile('4sq_data/checking_local_dedup.txt')
     local_place = sc.textFile('4sq_data/local_place.txt')
     venue_info = sc.textFile('4sq_data/venue_info.txt')
@@ -49,35 +51,48 @@ if __name__ == '__main__'"
 
     d = checking_place_id.leftOuterJoin(place_name)
     result1 = d.sortBy(lambda x: x[1][0], ascending=False)
-    print('time:%.2f' % (time.time() - ts))
 
-    # result1.take(10)
+    output += 'Lists the top checked-in venues (most popular):\n'
+    output += 'time:%.2f\n' % (time.time() - ts)
+    for i in result1.take(10):
+        output += str(i) + '\n'
+    output += line
 
     ts = time.time()
     categories = venue_info.map(row_split2).distinct().join(result1).map(lambda item: (
         item[1][0], item[1][1][0])).reduceByKey(add).sortBy(lambda item: item[1], ascending=False)
-    print('time:%.2f' % (time.time() - ts))
 
-    categories.take(10)
+    output += 'Lists the most popular categories:\n'
+    output += 'time:%.2f\n' % (time.time() - ts)
+    for i in categories.take(10):
+        output += str(i) + '\n'
+    output += line
 
     ts = time.time()
     user = checking_local_dedup.map(lambda line: (line.split(',')[0], 1)).reduceByKey(
         add).sortBy(lambda item: item[1], ascending=False)
-    print('time:%.2f' % (time.time() - ts))
-
-    # user.take(10)
+    output += 'Lists the top checked-in users:\n'
+    output += 'time:%.2f\n' % (time.time() - ts)
+    for i in user.take(10):
+        output += str(i) + '\n'
+    output += line
 
     ts = time.time()
     checkin_time = checking_local_dedup.map(convert_stamp).reduceByKey(
         add).sortBy(lambda item: item[1], ascending=False)
-    print('time:%.2f' % (time.time() - ts))
-
-    # checkin_time.take(20)
+    output += 'Lists the most popular time for check-ins:\n'
+    output += 'time:%.2f\n' % (time.time() - ts)
+    for i in checkin_time.take(20):
+        output += str(i) + '\n'
+    output += line
 
     ts = time.time()
     countries = venue_info.map(get_name).reduceByKey(
         add).sortBy(lambda item: item[1], ascending=False)
-    print('time:%.2f' % (time.time() - ts))
 
+    output += 'Optional functions:\n'
+    output += 'time:%.2f\n' % (time.time() - ts)
     for item in countries.take(5):
-        print('%s [%d]' % (geolocator.reverse(item[0]), item[1]))
+        output += '%s [%d]\n' % (geolocator.reverse(item[0]), item[1])
+    print(output, file=text_file)
+    text_file.close()
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Spark-Exercise
-NTUT BDA HW3
+NTUT BDA HW3 103590450
+* version python3.6
+* you need geopy and findspark
+* 如果沒有要在notebook執行就不需安裝findspark
+
+```sh
+$ pip3 install geopy findspark
+$ spark-submit HW3.py
+```
